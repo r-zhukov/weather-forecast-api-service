4e7ed27f6ed76995f60a2f20e31c2c84a8baadbc
Roman Zhukov <roman@advoncommerce.com>
2025-05-13 23:05:00 +0300

    set app port

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
