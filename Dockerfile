529ef9fe064256058711f3d183cbc4db1756893b
Roman Zhukov <roman@advoncommerce.com>
2025-05-13 22:05:40 +0300

    basic app config

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:18.0.0
+
+WORKDIR /app
+
+COPY package.json package.json
+COPY package-lock.json package-lock.json
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+CMD ["npm", "run", "start:prod"]
+
+
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
